7e307d5ce853157916eb0bc22c6d5a05f15e4c96
lenscas <lenscas@gmail.com>
2020-04-27 22:18:39 +0200

    impl std::error::Error for Error

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,18 @@ pub enum Method {
     Delete,
 }
 
+#[derive(Debug)]
 pub enum Error {
     BadHeader(&'static str),
 }
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::BadHeader(x) => x.fmt(f),
+        }
+    }
+}
+impl std::error::Error for Error {}
 
 pub struct Config<I: serde::Serialize> {
     pub url: String,
